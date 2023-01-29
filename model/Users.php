451a3ce06433d44aecb085d03ed84b2b9a589873
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-01-29 15:12:51 +0100

    Adding registration

--- model/Users.php
+++ model/Users.php
@@ -3,6 +3,26 @@ include '../model/Dbconnect.php';
 
 class Users extends DbConnect { 
 
+    protected function getAllUsers(){
+
+        try {
+            $sql = "SELECT id, username FROM users";
+    
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute();
+    
+            $results = $stmt->fetchAll();
+    
+            return $results;
+    
+    }  catch (Exception $e) {
+    
+            echo 'couldnt connect to db';
+    
+    }
+    
+    }
+
     protected function getUser($username){
 
         try {
@@ -21,5 +41,30 @@ class Users extends DbConnect {
     
     }
     
+    }
+
+
+    protected function addUser($username, $password){
+
+        try{
+
+            $encr_pw = password_hash($password, PASSWORD_BCRYPT);
+
+            $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+
+            $prep = $this->connect()->prepare($sql);
+
+            $prep->bindParam(':username', $username);
+            $prep->bindParam(':password', $encr_pw );
+
+            $prep->execute();
+
+            var_dump('done');
+
+            } catch (exception $e){
+            
+            var_dump('rolling back');
+            
+            }
     }
 }
