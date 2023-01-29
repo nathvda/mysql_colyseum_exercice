efcb178d59c709b1494afc98d722e9bdfd00bc43
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-01-29 20:50:14 +0100

    scripts

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,2 +1,11 @@
 import { menuBurger } from "./modules/menu_burger.js";
 
+
+window.addEventListener('mouseover', (e) => {
+
+console.log(e.clientX);
+console.log(e.clientY);
+
+document.getElementById('heroimg').style.boxShadow = `#000000 ${e.clientX*0.1}px ${e.clientY*0.1}px 0px`;
+
+})
